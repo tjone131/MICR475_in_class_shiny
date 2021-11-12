d1bd3c60f654b457ea03fa04b1954c5eda2f2a72
Ruth Simberloff <rsimberloff@gmail.com>
2021-11-12 11:36:39 -0500

    Annotated ggmap code

--- bird_app.R
+++ bird_app.R
@@ -1,8 +1,8 @@
 library(shiny)
 library(tidyverse)
 library(ggmap)
-WSKO <- read.csv("territorymapexample.csv")
-register_google(key = "AIzaSyAinMZGtkgpmpXIYJguj5K08Ja0sBv6cr4")
+WSKO <- read.csv("territorymapexample.csv") ## location data from WS/KO, one sparrow next to the Golden Gate Bridge
+register_google(key = "AIzaSyAinMZGtkgpmpXIYJguj5K08Ja0sBv6cr4") ## register API key to access Google maps through ggmap
 
 ui <- fluidPage(
                 titlePanel("Kernal Density Home Range of a Specific Sparrow"),
@@ -19,15 +19,15 @@ ui <- fluidPage(
 
 server <- function(input, output){
   output$densityplot <- renderPlot({
-    map <- ggmap(get_googlemap(center = c(lon = -122.477087, lat = 37.80741702),
-                               zoom = 20, scale = 2,
+    map <- ggmap(get_googlemap(center = c(lon = -122.477087, lat = 37.80741702), ## get a base map from Google, specify map center
+                               zoom = 20, scale = 2, # specify map scale
                                maptype ='satellite',
                                color = 'color')) +
       labs(x = "Longitude", y = "Latitude") +
       theme(axis.text.x =  element_text(angle = 45))
 
-    map + stat_density_2d(aes(x = Longitude, y = Latitude, fill = ..level..),
-                          alpha = .6, h = input$h, n = 90,
+    map + stat_density_2d(aes(x = Longitude, y = Latitude, fill = ..level..), # make a heat map on top of the Google map
+                          alpha = .6, h = input$h, n = 90, # h is the kernel width (smoothing factor), n is number of locations sampled
                           data=WSKO, geom = "polygon")+
       scale_fill_viridis_b()
     })
